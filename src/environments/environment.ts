35f9d7446c670c1432c8fe401f2b5c607fed986f
Artur Chilingaryan <1995.chilingaryan@gmail.com>
2024-11-29 15:44:58 +0400

    production envirnoment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,10 +3,10 @@ import {getEndpoints} from "./endpoints";
 export const environment = {
   production: true,
   tUserId: '5344748542',
-  communityChannelLink: "https://t.me/+rmvNDW3MDwI4NjJi",
-  apiUrl: "http://localhost:3000",
-  appUrl: 'https://d49d9ed820bd.ngrok.app',
-  botUrl: "https://t.me/lav_ches_ara_bot" // should be replaced
+  communityChannelLink: "https://t.me/tibtonic",
+  apiUrl: "https://api.tibtonic.org",
+  appUrl: 'https://app.tibtonic.org',
+  botUrl: "https://t.me/tibtonic_bot"
 };
 
 export const endpoints = getEndpoints(environment.apiUrl);
