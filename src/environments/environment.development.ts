dcf6a2b93bd47bc48f509b2f6691b485c622143f
Artur Chilingaryan <1995.chilingaryan@gmail.com>
2024-12-04 23:06:16 +0400

    monorepo integration

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,9 +4,8 @@ export const environment = {
   production: false,
   tUserId: '5344748542',
   communityChannelLink: "https://t.me/+rmvNDW3MDwI4NjJi",
-  apiUrl: "https://c0cac7af2c0a.ngrok.app",
-  appUrl: 'https://ff46fccb512c.ngrok.app',
+  appUrl: 'https://2de1f742ad83.ngrok.app',
   botUrl: "https://t.me/toon_miner_local_bot",
 };
 
-export const endpoints = getEndpoints(environment.apiUrl);
+export const endpoints = getEndpoints(`${environment.appUrl}/api`);
